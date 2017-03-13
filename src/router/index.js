fdd412c41c6f4eee65b8978939630fa9434fe728
cy <xiongcaihu@qq.com>
2017-03-13 18:24:58 +0800

    12312

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import my from '../App.vue'
+import hi from '../components/test.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [{
     path: '/',
-    component: my
+    component: hi
   }]
 })
