d3d83aadfaba54c1739b49e4fea5b1c722f3ee71
windOnTheSea <xiongcaihu@qq.com>
2017-03-28 16:01:43 +0800

    更新 App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,189 +1,229 @@
 <template>
-    <div id="app">
-        <button @click="pirntList()">print list</button>
-        <!--<drag :your-list="myList" :baseWidth="10" :baseHeight="5">
-            <div class="chart" v-for="(item,index) in myList" :slot="'slot'+index" :ref="'chart'+index">
-                {{item.id}}
-            </div>
-        </drag>-->
-        <drag :your-list="myList" :baseWidth="100" :baseHeight="50" ref="drag">
-            <div class="chart" v-for="(item,index) in myList" :slot="'slot'+index" :ref="'chart'+index">
-                {{item.id}}
-            </div>
-        </drag>
+    <div id="demo">
+        <div class="head">
+            <span @click="addChart()">Add Chart</span>
+            <span @click="addItemBox()">Add Item Box</span>
+        </div>
+        <cy-gridster ref="cyGridster" :your-list="myList" :base-width="100" :base-height="100" :drag-start="dragStart" :dragging="dragging"
+            :drag-end="dragEnd" :resize-start="resizeStart" :resizing="resizing" :resize-end="resizeEnd">
+            <template v-for="(item,index) in myList" :slot="'slot'+index">
+                <div class="dragHandle">
+                    <div class="tool">
+                        <span @click="deleteItem(index)">Delete This Item</span>
+                    </div>
+                    <div class="chart" :ref="'chart'+index"></div>
+                </div>
+            </template>
+        </cy-gridster>
     </div>
 </template>
 
 <script>
     var Highcharts = require('highcharts');
 
-    // 在 Highcharts 加载之后加载功能模块
-    require('highcharts/modules/exporting')(Highcharts);
+    let chartInstanceBox = {} //乘装highchart实例的对象
 
-    import drag from './components/drag7/drag.vue';
+    // 在 Highcharts 加载之后加载功能模块
+    // require('highcharts/modules/exporting')(Highcharts);
+
+    import drag from './components/vueGridster/drag.vue';
     import mock from "mockjs"
+    import _ from "lodash";
 
     export default {
         data() {
             let list = mock.mock({
-                // "myList|100": [{
-                //     "id|+1": 1,
-                //     "text|+1": 1,
-                //     x: "@integer(1,20)",
-                //     y: "@integer(1,20)",
-                //     sizex: "@integer(10,10)",
-                //     sizey: "@integer(10,10)",
-                // }],
-                "myList|5": [{
+                "myList|1-3": [{
                     "id|+1": 1,
-                    "text|+1": 1,
-                    x: "@integer(1,5)",
-                    y: "@integer(1,5)",
-                    sizex: "@integer(1,3)",
-                    sizey: "@integer(1,3)",
+                    x: 1,
+                    y: 1,
+                    sizex: 6,
+                    sizey: 6,
+                }, {
+                    "id|+1": 3,
+                    x: 7,
+                    y: 1,
+                    sizex: 6,
+                    sizey: 3,
+                }, {
+                    "id|+1": 4,
+                    x: 7,
+                    y: 1,
+                    sizex: 6,
+                    sizey: 3,
                 }]
             })
             return {
                 myList: list.myList,
-                // myList: [
-                //     {
-                //     id: 1,
-                //     x: 1,
-                //     y: 1,
-                //     sizex: 1,
-                //     sizey: 1
-                // }, {
-                //     id: 2,
-                //     x: 1,
-                //     y: 2,
-                //     sizex: 1,
-                //     sizey: 1
-                // }, 
-                // ]
-                // myList: [{
-                //     "id": 1,
-                //     "text": 1,
-                //     "x": 2,
-                //     "y": 2,
-                //     "sizex": 2,
-                //     "sizey": 2
-                // }, {
-                //     "id": 2,
-                //     "text": 2,
-                //     "x": 4,
-                //     "y": 4,
-                //     "sizex": 1,
-                //     "sizey": 2
-                // }, {
-                //     "id": 3,
-                //     "text": 3,
-                //     "x": 4,
-                //     "y": 3,
-                //     "sizex": 2,
-                //     "sizey": 2
-                // }, {
-                //     "id": 4,
-                //     "text": 4,
-                //     "x": 3,
-                //     "y": 2,
-                //     "sizex": 2,
-                //     "sizey": 1
-                // }, {
-                //     "id": 5,
-                //     "text": 5,
-                //     "x": 3,
-                //     "y": 3,
-                //     "sizex": 2,
-                //     "sizey": 2
-                // }]
-                // myList:[
-                //     {
-                //         id:1,
-                //         x:1,
-                //         y:3,
-                //         sizex:1,
-                //         sizey:1
-                //     },
-                //     {
-                //         id:2,
-                //         x:1,
-                //         y:1,
-                //         sizex:1,
-                //         sizey:2
-                //     },
-                //     {
-                //         id:3,
-                //         x:1,
-                //         y:1,
-                //         sizex:1,
-                //         sizey:3
-                //     },
-                // ]
             }
         },
         components: {
-            "drag": drag
+            'cy-gridster': drag
         },
         name: 'app',
         methods: {
-            pirntList() {
-                console.dir(JSON.stringify(this.$refs['drag'].getList()));
+            addChart() {
+                let vm = this;
+                let gridster = this.$refs['cyGridster']; //获取gridster实例
+
+                gridster.addItemBox({
+                    id: 1111,
+                    x: 1,
+                    y: 1,
+                    sizex: 6,
+                    sizey: 2
+                })
+
+                gridster.$nextTick(function () {
+                    //调用addItemBox后，会在this.myList中增加一个item，而增加的位置，每次都是最后
+                    let chart=new Highcharts.chart(vm.$refs['chart' + (vm.myList.length - 1)][0], mock.mock({
+                        chart:{
+                            'type|1':['line','spline','column']
+                        },
+                        title:{
+                            align:'left',
+                            text:'@title(2,3)'
+                        },
+                        credits: {
+                            enabled: false
+                        },
+                        series: [{
+                            'data|1-10': ['@integer(1,100)']
+                        }]
+                    }));
+
+                    chartInstanceBox['chart'+(vm.myList.length-1)]=chart;
+                })
+            },
+            addItemBox() {
+                let gridster = this.$refs['cyGridster']; //获取gridster实例
+
+                gridster.addItemBox({
+                    id: 1111,
+                    x: 1,
+                    y: 1,
+                    sizex: 6,
+                    sizey: 2
+                })
+            },
+            deleteItem(index) {
+                let gridster = this.$refs['cyGridster']; //获取gridster实例
+
+                gridster.removeItem(index); //此时会在this.myList的index位置将item置为{}，目的是为了不让vue重新渲染整个v-for。
+            },
+            /**
+                e:事件对象
+                item：拖动对象
+                index：拖动对象下标
+             */
+            dragStart(e, item, index) {
+            },
+            dragging(e, item, index) {
+            },
+            dragEnd(e, item, index) {
+            },
+            resizeStart(e, item, index) {
+            },
+            resizing(e, item, index) {
+            },
+            resizeEnd(e, item, index) {
+                chartInstanceBox['chart'+index].reflow();        
             }
         },
         mounted() {
-            return;
             let vm = this;
-            this.$nextTick(function () {
-                _.forEach(this.myList, function (item, index) {
+
+            let gridster = vm.$refs['cyGridster']; //获取gridster实例
+            gridster.afterInitOk(function () {
+                _.forEach(vm.myList, function (item, index) {
                     let chartNode = vm.$refs['chart' + index][0];
                     // 创建图表
-                    Highcharts.chart(chartNode, {
+                    let chart = new Highcharts.chart(chartNode, mock.mock({
                         chart: {
-                            type: 'bar'
+                            "type|1": ['bar', 'column', 'line', 'spline']
                         },
                         title: {
-                            text: '堆叠条形图'
-                        },
-                        xAxis: {
-                            categories: ['苹果', '橘子', '梨', '葡萄', '香蕉']
-                        },
-                        yAxis: {
-                            min: 0,
-                            title: {
-                                text: '总的水果消费'
-                            }
-                        },
-                        legend: {
-                            reversed: true
+                            align: 'left',
+                            text: '@title(1,3)'
                         },
                         plotOptions: {
-                            series: {
-                                stacking: 'normal'
+                            line: {
+                                marker: {
+                                    enabled: false
+                                }
                             }
                         },
-                        series: [{
-                            name: '小张',
-                            data: [5, 3, 4, 7, 2]
-                        }, {
-                            name: '小彭',
-                            data: [2, 2, 3, 2, 1]
-                        }, {
-                            name: '小潘',
-                            data: [3, 4, 4, 2, 5]
+                        xAxis: {
+                            "categories|5": ['@city()']
+                        },
+                        credits: {
+                            enabled: false
+                        },
+                        "series|1-3": [{
+                            "name": '@region()',
+                            "data|5": ['@integer(1,100)']
                         }]
-                    });
-                })
+                    }));
+
+                    chartInstanceBox['chart' + index] = chart;
+                });
             })
         }
     }
 
 </script>
 
-<style>
-    .chart {
+<style lang='less'>
+    body {
+        overflow-x: hidden;
+        & * {
+            box-sizing: border-box;
+        }
+    }
+
+    #demo {
         width: 100%;
-        height: 100%;
-        overflow: hidden;
+        padding: 1.5rem 1.5rem 1.5rem 0;
+
+        .head {
+            position: absolute;
+            left: .5rem;
+            top: 1rem;
+            z-index:9;
+
+            span {
+                cursor: pointer;
+                font-weight: bold;
+                margin-left: 1rem;
+            }
+        }
+    }
+
+    //拖动布局容器样式
+    .dragAndResize {
+        width: e('calc(100% - 1.5rem)');
+        //布局框样式
+        .item {}
+        
+        .dragHandle {
+            //拖动手柄样式
+            padding: 1.5rem!important;
+            height: 100%; //自定义内容样式
+            .tool {
+                position: absolute;
+                right: .5rem;
+                top: .5rem;
+                cursor: pointer;
+                font-weight: bold;
+            }
+
+            .chart {
+                width: 100%;
+                height: 100%;
+                overflow: hidden;
+                cursor: default;
+            }
+        }
     }
 
 </style>
