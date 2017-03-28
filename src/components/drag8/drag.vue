7e489e2c52f2dbeb5202b9b145a9e266407d011c
windOnTheSea <xiongcaihu@qq.com>
2017-03-28 16:00:54 +0800

    新建 drag8/drag.vue

---
+++ src/components/drag8/drag.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class='dragAndResize' ref='container' @mousedown="containerMouseDown($event)" @mouseup="endMove($event)" @mousemove="moving($event)">
+        <div v-if="renderOk">
+            <div :class="{item:true,moveAnimation:moveAnimate,movingItem:item.isPlayer,canNotDrag:!draggable}" @mousedown="startMove($event,item,index)" :ref="'item'+index" v-for="(item,index) in yourList"
+                :key="'item'+index" :style="nowItemStyle(item,index)">
+                <slot :name="'slot'+index"></slot>
+                <span class="resizeHandle" v-show="resizable" @mousedown="startResize($event,item,index)"></span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import js from "./drag.js"
+    export default js
+
+</script>
+
+<style lang="less">
+    @import "./drag.less";
+
+</style>
