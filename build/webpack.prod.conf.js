e6a889938b9715b9aa17bb2ba2afb6998a86d4b8
cy <xiongcaihu@qq.com>
2017-03-31 19:17:34 +0800

    adsadasdas

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -31,6 +31,10 @@ var webpackConfig = merge(baseWebpackConfig, {
     new webpack.DefinePlugin({
       'process.env': env
     }),
+    new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery :'jquery'
+        }),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         warnings: false
